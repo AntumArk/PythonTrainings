e6dc6a3579613eb0cab83d97016d2a657d73f2c7
Linas Minkevicius <minkevicius.linas@ext.fmc-ag.com>
2023-09-18 15:55:31 +0300

    solver checks all grid

--- TicTacToeSolver.py
+++ TicTacToeSolver.py
@@ -1,5 +1,34 @@
 def solve_ticTacToe(cells):
     print("Solving")
+    check_crosses(cells)
+    check_rows(cells)
+    check_columns(cells)
+    print("tie")
+
+
+def check_rows(cells):
+    print("Checking Rows")
+    grid_size=3
+    first_index = 0
+    for cycle in range(grid_size):
+        sum = cells[first_index] + cells[first_index+1] + cells[first_index+2]
+        print("Sum ", sum)
+        if check_win(sum):
+           return
+        first_index+=grid_size
+         
+def check_columns(cells):
+    print("Checking Columns")
+    grid_size=3
+    for cycle in range(grid_size):
+        sum = cells[cycle] + cells[cycle+grid_size] + cells[cycle+2*grid_size]
+        print("Sum ", sum)
+        if check_win(sum):
+           return
+# end def
+
+def check_crosses(cells):
+    print("Checking crosses")
     center_index = 4
     first_point = 0
     last_point = 8
@@ -8,13 +37,17 @@ def solve_ticTacToe(cells):
         print("cycle ", cycle)
         sum = cells[first_point] + cells[center_index] + cells[last_point]
         print("Sum ", sum)
-        if sum == 3:
-            print("X WON!")
-            return
-        if sum == -3:
-            print("O WON!")
+        if check_win(sum):
             return
         # Rotate beam
         first_point += 1
         last_point -= 1
-    print("tie")
+
+def check_win(sum: int)-> bool: 
+    if sum == 3:
+        print("X WON!")
+        return True
+    if sum == -3:
+        print("O WON!")
+        return True
+    return False

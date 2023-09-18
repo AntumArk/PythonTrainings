6aac159f4afb101544887dbbb7d05bd52c6ccddf
Linas Minkevicius <minkevicius.linas@ext.fmc-ag.com>
2023-09-18 17:02:39 +0300

    Basic inputs and player switching

--- Controller.py
+++ Controller.py
@@ -1,5 +1,5 @@
 from grid import TicTacGrid, TicTacValues
-
+from TicTacToeSolver import solve_ticTacToe, check_board_full
 class GameController:
     game_cells = [
     TicTacValues.Z,    TicTacValues.Z,    TicTacValues.Z,
@@ -18,10 +18,24 @@ class GameController:
         if self.current_player == TicTacValues.Z:
             print("Invalid player cant play.")
             return
+        self.play()
+        
+        
+    def play(self):
         self.UI.draw_grid(self.game_cells)
-        
-        
+        while(not check_board_full(self.game_cells)):# and solve_ticTacToe(self.game_cells)==TicTacValues.Z):
+            self.UI.draw_grid(self.game_cells)
+            print("Select which cell to enter[1-9]. Current player:", self.current_player)
+            line = input()
+            self.switch_player()
+        print("GAME OVER")
 
+    def switch_player(self):
+        if self.current_player==TicTacValues.X:
+            self.current_player=TicTacValues.O
+        else:
+            self.current_player=TicTacValues.X
+            
     def get_player_from_input(self):
         print("Select which player starts. Enter \"X\" or \"Y\"")
         line=input()

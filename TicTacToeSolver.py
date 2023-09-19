043f214ca703c8cacc5d6b38a8518fbfa324268c
Linas Minkevicius <minkevicius.linas@ext.fmc-ag.com>
2023-09-19 14:52:34 +0300

    updated cross solution calculation withouh loops

--- TicTacToeSolver.py
+++ TicTacToeSolver.py
@@ -45,25 +45,25 @@ def check_columns(cells: list[TicTacValues], grid_size: int) -> TicTacValues:
 
 
 def check_crosses(cells: list[TicTacValues], grid_size: int) -> TicTacValues:
-
     if grid_size % 2 == 0:
         print("Not solving crosses for even grid")
         return TicTacValues.Z
 
-    print("Checking crosses")    
-    last_point = grid_size * 2 - 1
-    center_index = int(last_point / 2)
-    first_point = 0
-    crosses_to_solve = 4
-    for _ in range(crosses_to_solve):
-        cell_sum = cells[first_point] + cells[center_index] + cells[last_point]
-        print("cell_sum ", cell_sum)
-        result = check_win(cell_sum)
-        if result != TicTacValues.Z:
-            return result
-        # Rotate beam
-        first_point += 1
-        last_point -= 1
+    # Primary principal
+    print("Checking crosses")
+    cell_sum = sum(cells[0 :: (grid_size - 1) + 2])
+    print("cell_sum ", cell_sum)
+    result = check_win(cell_sum)
+    if result != TicTacValues.Z:
+        return result
+
+    # Secondary principal
+    cell_sum = sum(cells[grid_size - 1 :: grid_size])
+    print("cell_sum ", cell_sum)
+    result = check_win(cell_sum)
+    if result != TicTacValues.Z:
+        return result
+
     return TicTacValues.Z
 
 
